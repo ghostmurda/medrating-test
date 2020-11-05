fa293bd85c4ded4f2decabd4f0ca586efb539330
Daniel Simonov <sk8kmd@gmail.com>
2020-11-05 19:25:50 +0300

    Update markup, update store

--- src/js/headerLinks.js
+++ src/js/headerLinks.js
@@ -1,4 +1,5 @@
 import {store} from "./index.js";
+import {CATALOG, FAVORITES} from "./index.js";
 
 const catalogLink = document.querySelector('.header__link_type-catalog');
 const favoritesLink = document.querySelector('.header__link_type-favorites');
@@ -6,11 +7,11 @@ const favoritesLink = document.querySelector('.header__link_type-favorites');
 catalogLink.addEventListener('click', () => {
     catalogLink.classList.add('active');
     favoritesLink.classList.remove('active');
-    store.pageType = 'catalog';
+    store.pageType = CATALOG;
 })
 
 favoritesLink.addEventListener('click', () => {
     favoritesLink.classList.add('active');
     catalogLink.classList.remove('active');
-    store.pageType = 'favorites';
+    store.pageType = FAVORITES;
 })
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,39 @@
+export const CATALOG = 'catalog';
+export const FAVORITES = 'favorites';
+
+const catalogPage = document.querySelector('.content_type-catalog');
+const favoritesPage = document.querySelector('.content_type-favorites');
+
 const pageState = {
-    pageType: 'catalog'  // catalog || favorites
+    pageType: CATALOG
 }
 
 export const store = new Proxy(pageState, {
     set: function(target, key, value) {
-        console.log(`The property ${key} has been updated with ${value}`);
+        subscribeStore(value);
         return true;
     }
 })
+
+const showPage = (page) => {
+    switch (page) {
+        case CATALOG:{
+            catalogPage.classList.remove('hide');
+            favoritesPage.classList.add('hide');
+            break
+        }
+        case FAVORITES: {
+            favoritesPage.classList.remove('hide');
+            catalogPage.classList.add('hide');
+            break
+        }
+        default:
+            catalogPage.classList.remove('hide');
+            favoritesPage.classList.add('hide');
+            break
+    }
+}
+
+function subscribeStore(state){
+    showPage(state);
+}
