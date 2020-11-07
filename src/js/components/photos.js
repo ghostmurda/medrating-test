51dc8d24665ace75a56f3cc3ff504b79cd52e9e3
Daniel Simonov <sk8kmd@gmail.com>
2020-11-07 19:53:55 +0300

    Add modal window, update styles

--- src/js/components/photos.js
+++ src/js/components/photos.js
@@ -5,10 +5,32 @@ export default class Photos{
 
     addListener(){
         this.photos.forEach(photo => {
-             // photo.addEventListener('mouseenter', event => {
-             //     console.log('enter' + event.target.url);
-             //     event.stopPropagation();
-             // })
+             photo.addEventListener('click', event => {
+                 let overlay = document.querySelector('.overlay');
+                 let modalWindow = document.querySelector('.modal-window');
+                 let modalImg = modalWindow.querySelector('.modal-window__img');
+
+                 new Promise(resolve => {
+                     overlay.style.display = 'block';
+                     setTimeout(() => resolve(), 10);
+                 }).then(() => {
+                     overlay.style.opacity = '1';
+                     modalWindow.style.display = 'flex';
+                     modalImg.src = photo.url;
+
+                     let listener = () => {
+                         overlay.style.display = 'none';
+                         modalWindow.style.display = 'none';
+                         overlay.style.opacity = '0';
+                         modalImg.src = '';
+                         window.removeEventListener('click', listener);
+                     }
+
+                     window.addEventListener('click', listener);
+                 })
+
+                 event.stopPropagation();
+             })
         })
     }
 }
