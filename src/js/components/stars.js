8b1bfda96c9df8b41813ee312f767f5fd989f17f
Daniel Simonov <sk8kmd@gmail.com>
2020-11-07 18:59:31 +0300

    Update styles, update stars class, update markup

--- src/js/components/stars.js
+++ src/js/components/stars.js
@@ -10,6 +10,13 @@ export default class Stars{
                 if (star.classList.contains('star_selected')){
                     star.classList.remove('star_selected');
                     localStorage.removeItem(img.id);
+                    if (star.parentNode.closest('.content_type-favorites')){
+                        new Promise(resolve => {
+                            img.style.opacity = '0';
+                            star.style.opacity = '0';
+                            setTimeout(() => resolve(), 1000)
+                        }).then(() => star.parentNode.remove());
+                    }
                 } else {
                     star.classList.add('star_selected');
                     localStorage.setItem(img.id, JSON.stringify({
